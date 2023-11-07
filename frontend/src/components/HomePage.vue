28b4d7fc1423fe838f3e71d5b568c868349963b0
Andy <andresreinoza1@gmail.com>
2023-11-07 10:11:18 -0300

    Creation of hero section

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,46 +1,41 @@
 <template>
-  <div class="container mt-5">
-    <div v-for="article in articles" :key="article.id">
-        <h3>{{article.title}}</h3>
-        <p>{{article.body}}</p>
-        <p>{{article.fecha}}</p>
-    </div>
-  </div>
+ <VideoHero/>
 </template>
 
 <script>
+import VideoHero from './VideoHero.vue'
 export default {
-
-  data(){
-    return {
-      articles:[],
-    }
-  },
-    methods:{
-      getArticles(){
-        fetch('http://localhost:5000/articles',{
-          method:"GET",
-          headers:{
-            'Content-Type': 'application/json'
-          }
-        })
-        .then(resp => resp.json())
-        .then(data => {
-          let articulos = data.articles;
-          console.log(articulos);
-          articulos.forEach(articulo => {this.articles.push(articulo)})
-          //this.articles.push(...data)
-        })
-        .catch(error =>{
-          console.log(error);
-        })
-      }
+    data() {
+        return {
+            articles: [],
+        };
+    },
+    methods: {
+        getArticles() {
+            fetch('http://localhost:5000/articles', {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(resp => resp.json())
+                .then(data => {
+                let articulos = data.articles;
+                console.log(articulos);
+                articulos.forEach(articulo => { this.articles.push(articulo); });
+                //this.articles.push(...data)
+            })
+                .catch(error => {
+                console.log(error);
+            });
+        }
     },
     //este se llama una vez creado el objeto, ver lifecicle hooks
-    created(){
-      this.getArticles();
-    }
-  }
+    created() {
+        this.getArticles();
+    },
+    components: { VideoHero }
+}
 </script>
 
 <style>
---
+++ frontend/src/components/VideoHero.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="hero--section" >
+        <video src="../assets/video/Concert_Video.mp4" autoplay controls loop></video>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+video{
+    width: 100vw;
+}
+</style>
